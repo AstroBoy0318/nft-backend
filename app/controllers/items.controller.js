77f927948404b38f99b3a28cd5a4fd1b15d3d0d2
toby0318 <78232000+toby0318@users.noreply.github.com>
2021-12-31 11:25:01 +0900

    updated for ERC-1155
    Added amount field

--- app/controllers/items.controller.js
+++ app/controllers/items.controller.js
@@ -15,7 +15,7 @@ exports.all = (req, res) => {
 
     if (req.query.search != undefined && req.query.search != '') {
         query.name = {
-            [Op.like]: '%'+req.query.search+'%'
+            [Op.like]: '%' + req.query.search + '%'
         }
     }
     if (req.query.owner != undefined && req.query.owner != '') {
@@ -24,7 +24,7 @@ exports.all = (req, res) => {
     if (req.query.collectionId != undefined && req.query.collectionId != '') {
         query.collectionId = req.query.collectionId;
     }
-    if( req.query.category != undefined && req.query.category != '' && req.query.category > 0 ) {
+    if (req.query.category != undefined && req.query.category != '' && req.query.category > 0) {
         query.category = req.query.category;
     }
     condition.length = req.query.limit * 1;
@@ -46,25 +46,24 @@ exports.all = (req, res) => {
 
 
     Items.findAll({
-        where : query,
-        limit: condition.length,
-        offset: condition.start,
-        order: mysort,
-        include: [
-            {
-                model: Collections
-            },
-            {
-                model: Accounts
-            }
-        ]
-    })
+            where: query,
+            limit: condition.length,
+            offset: condition.start,
+            order: mysort,
+            include: [{
+                    model: Collections
+                },
+                {
+                    model: Accounts
+                }
+            ]
+        })
         .then(datas => {
-            res.status(200).send({items: datas});
+            res.status(200).send({ items: datas });
         })
         .catch(err => {
             console.log(err);
-            res.status(500).send({items: []});
+            res.status(500).send({ items: [] });
         });
 };
 
@@ -77,6 +76,7 @@ exports.add = (req, res) => {
         tokenId: req.body.tokenId,
         txHash: req.body.txHash,
         metadata: req.body.metadata,
+        amount: req.body.amount,
         external_link: req.body.external_link,
         description: req.body.description,
         image: req.body.image,
@@ -86,63 +86,62 @@ exports.add = (req, res) => {
         creator: req.body.creator,
         owner: req.body.owner,
     }).then(res_data => {
-        return res.status(200).send({ status: 'success', message: 'NFT item is successfully added'});
+        return res.status(200).send({ status: 'success', message: 'NFT item is successfully added' });
     }).catch(err => {
-        return res.status(500).send({ status: 'fail', message: err.message});
+        return res.status(500).send({ status: 'fail', message: err.message });
     })
 }
 
 exports.update = (req, res) => {
     let item = req.body;
     Items.update(
-        item,
-        {where: {id: req.params.id}}
-    )
-    .then(res_data => {
-        return res.status(200).send({ status:'success', message: "NFT item is successfully updated" });
-    })
-    .catch(err => {
-        return res.status(500).send({ status:'fail', message: err.message });
-    });
+            item, { where: { id: req.params.id } }
+        )
+        .then(res_data => {
+            return res.status(200).send({ status: 'success', message: "NFT item is successfully updated" });
+        })
+        .catch(err => {
+            return res.status(500).send({ status: 'fail', message: err.message });
+        });
 }
 
 exports.one = (req, res) => {
     Items.findOne({
-        where: {
-            collectionId: req.params.collectionId,
-            tokenId: req.params.tokenId,
-        }
-    })
-    .then(async result => {
-        let creatorObj = await Accounts.findOne({where: {address: result.creator.toLowerCase()}});
-        let ownerObj = await Accounts.findOne({where: {address: result.owner.toLowerCase()}});
-        let category = await Categories.findOne({where: {id: result.category}});
-        console.log(ownerObj)
-        result.creatorObj = creatorObj;
-        result.ownerObj = ownerObj;
-        let data = {
-            id: result.id,
-            category: category.name,
-            tokenId: result.tokenId,
-            collectionId: result.collectionId,
-            name: result.name,
-            price: result.price,
-            mint_price: result.mint_price,
-            assetType: result.assetType,
-            metadata: result.metadata,
-            image: result.image,
-            creator: result.creator,
-            owner: result.owner,
-            description: result.description,
-            txHash: result.txHash,
-            createdAt: result.createdAt,
-            updatedAt: result.updatedAt,
-            creatorObj: creatorObj,
-            ownerObj: ownerObj,
-        };
-        res.status(200).send({item: data})
-    })
-    .catch(err => {
-        res.status(500).send({item: {}})
-    });
+            where: {
+                collectionId: req.params.collectionId,
+                tokenId: req.params.tokenId,
+            }
+        })
+        .then(async result => {
+            let creatorObj = await Accounts.findOne({ where: { address: result.creator.toLowerCase() } });
+            let ownerObj = await Accounts.findOne({ where: { address: result.owner.toLowerCase() } });
+            let category = await Categories.findOne({ where: { id: result.category } });
+            console.log(ownerObj)
+            result.creatorObj = creatorObj;
+            result.ownerObj = ownerObj;
+            let data = {
+                id: result.id,
+                category: category.name,
+                tokenId: result.tokenId,
+                collectionId: result.collectionId,
+                name: result.name,
+                price: result.price,
+                mint_price: result.mint_price,
+                assetType: result.assetType,
+                metadata: result.metadata,
+                image: result.image,
+                creator: result.creator,
+                owner: result.owner,
+                description: result.description,
+                txHash: result.txHash,
+                createdAt: result.createdAt,
+                updatedAt: result.updatedAt,
+                creatorObj: creatorObj,
+                ownerObj: ownerObj,
+            };
+            res.status(200).send({ item: data })
+        })
+        .catch(err => {
+            res.status(500).send({ item: {} })
+        });
 }
--- app/routes/items.routes.js
+++ app/routes/items.routes.js
@@ -2,209 +2,211 @@ const { authJwt } = require("../middleware");
 const controller = require("../controllers/items.controller");
 
 module.exports = function(app) {
-  
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
 
-  /**
-   * @swagger
-   * /items:
-   *   get:
-   *     summary: List all items
-   *     description: List all collections as an JSON array
-   *     tags: [Items]
-   *     produces:
-   *       - application/json
-   *     parameters:
-   *       - in: query
-   *         name: sortBy
-   *         schema:
-   *           type: string
-   *           description: sortBy
-   *       - in: query
-   *         name: page
-   *         schema:
-   *          type: number
-   *       - in: query
-   *         name: limit
-   *         schema:
-   *          type: number
-   *       - in: query
-   *         name: owner
-   *         schema:
-   *          type: string
-   *       - in: query
-   *         name: category
-   *         schema:
-   *          type: number
-   *       - in: query
-   *         name: sortDir
-   *         schema:
-   *          type: string
-   *       - in: query
-   *         name: search
-   *         schema:
-   *          type: string
-   *       - in: query
-   *         name: collectionId
-   *         schema:
-   *          type: string
-   *     responses:
-   *       200:
-   *         description: "successful operation"
-   */
+    app.use(function(req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
 
-  app.get("/api/v1/items", controller.all);
-  /**
-   * @swagger
-   * /items/add:
-   *   post:
-   *     summary: Add new records in items list
-   *     description: "This API is for Adding new records in items list"
-   *     tags: [Items]
-   *     produces:
-   *       - application/json
-   *     parameters:
-   *       - name: body
-   *         description: This contains Detail of Collections as JSON Object.
-   *         in: body
-   *         required: true
-   *         schema:
-   *          type: object
-   *          required:
-   *            - name
-   *            - tokenId
-   *            - metadata
-   *            - collectionId
-   *            - creator
-   *            - owner
-   *          properties:
-   *            name:
-   *              type: string
-   *            assetType:
-   *              type: string
-   *            category:
-   *              type: number
-   *            tokenId:
-   *              type: number
-   *            metadata:
-   *              type: string
-   *            txHash:
-   *              type: string
-   *            external_link:
-   *              type: string
-   *            description:
-   *              type: string
-   *            property:
-   *              type: string
-   *            mint_price:
-   *              type: number
-   *            price:
-   *              type: number
-   *            collectionId:
-   *              type: string
-   *            creator:
-   *              type: string
-   *            owner:
-   *              type: string
-   *
-   *
-   *     responses:
-   *       200:
-   *         description: This contain to the success message
-   *         schema:
-   *           type: object
-   *           properties:
-   *            message:
-   *              type: string
-   *       426:
-   *         description: "Indicate that version in header is not supported"
-   *       400:
-   *         description: "Indicate body parameters are not valid."
-   *       401:
-   *         description: "User don't have permissions to execute this API or token is not valid"
-   *       498:
-   *         description: "Indicate Auth token is invalid or Expired"
-   */
-  app.post("/api/v1/items/add", controller.add);
-  /**
-   * @swagger
-   * /items/update/{id}:
-   *   put:
-   *     summary: update item by id
-   *     description: This API is for updating.
-   *     tags: [Items]
-   *     produces:
-   *       - application/json
-   *     parameters:
-   *       - name: body
-   *         description: Application Version.
-   *         in: body
-   *         required: true
-   *         schema:
-   *          type: object
-   *          properties:
-   *            status:
-   *              type: number
-   *            name:
-   *              type: string
-   *            tokenId:
-   *              type: number
-   *            metadata:
-   *              type: string
-   *            txHash:
-   *              type: string
-   *            assetType:
-   *              type: string
-   *            external_link:
-   *              type: string
-   *            description:
-   *              type: string
-   *            property:
-   *              type: string
-   *            mint_price:
-   *              type: number
-   *            price:
-   *              type: number
-   *            collectionId:
-   *              type: string
-   *            creator:
-   *              type: string
-   *            owner:
-   *              type: string
-   *       - name: id
-   *         description: id of item to be updated.
-   *         in: path
-   *         required: true
-   *         type: string
-   *     responses:
-   *       200:
-   *         description: Response contain success message.
-   *         schema:
-   *           type: object
-   *           properties:
-   *            message:
-   *              type: string
-   *       426:
-   *         description: "Indicate that version in header is not supported"
+    /**
+     * @swagger
+     * /items:
+     *   get:
+     *     summary: List all items
+     *     description: List all collections as an JSON array
+     *     tags: [Items]
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - in: query
+     *         name: sortBy
+     *         schema:
+     *           type: string
+     *           description: sortBy
+     *       - in: query
+     *         name: page
+     *         schema:
+     *          type: number
+     *       - in: query
+     *         name: limit
+     *         schema:
+     *          type: number
+     *       - in: query
+     *         name: owner
+     *         schema:
+     *          type: string
+     *       - in: query
+     *         name: category
+     *         schema:
+     *          type: number
+     *       - in: query
+     *         name: sortDir
+     *         schema:
+     *          type: string
+     *       - in: query
+     *         name: search
+     *         schema:
+     *          type: string
+     *       - in: query
+     *         name: collectionId
+     *         schema:
+     *          type: string
+     *     responses:
+     *       200:
+     *         description: "successful operation"
+     */
 
-   *       404:
-   *         description: "Indicate driver_id is not valid."
+    app.get("/api/v1/items", controller.all);
+    /**
+     * @swagger
+     * /items/add:
+     *   post:
+     *     summary: Add new records in items list
+     *     description: "This API is for Adding new records in items list"
+     *     tags: [Items]
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: body
+     *         description: This contains Detail of Collections as JSON Object.
+     *         in: body
+     *         required: true
+     *         schema:
+     *          type: object
+     *          required:
+     *            - name
+     *            - tokenId
+     *            - metadata
+     *            - collectionId
+     *            - creator
+     *            - owner
+     *          properties:
+     *            name:
+     *              type: string
+     *            assetType:
+     *              type: string
+     *            category:
+     *              type: number
+     *            tokenId:
+     *              type: number
+     *            metadata:
+     *              type: string
+     *            amount:
+     *              type: number
+     *            txHash:
+     *              type: string
+     *            external_link:
+     *              type: string
+     *            description:
+     *              type: string
+     *            property:
+     *              type: string
+     *            mint_price:
+     *              type: number
+     *            price:
+     *              type: number
+     *            collectionId:
+     *              type: string
+     *            creator:
+     *              type: string
+     *            owner:
+     *              type: string
+     *
+     *
+     *     responses:
+     *       200:
+     *         description: This contain to the success message
+     *         schema:
+     *           type: object
+     *           properties:
+     *            message:
+     *              type: string
+     *       426:
+     *         description: "Indicate that version in header is not supported"
+     *       400:
+     *         description: "Indicate body parameters are not valid."
+     *       401:
+     *         description: "User don't have permissions to execute this API or token is not valid"
+     *       498:
+     *         description: "Indicate Auth token is invalid or Expired"
+     */
+    app.post("/api/v1/items/add", controller.add);
+    /**
+     * @swagger
+     * /items/update/{id}:
+     *   put:
+     *     summary: update item by id
+     *     description: This API is for updating.
+     *     tags: [Items]
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: body
+     *         description: Application Version.
+     *         in: body
+     *         required: true
+     *         schema:
+     *          type: object
+     *          properties:
+     *            status:
+     *              type: number
+     *            name:
+     *              type: string
+     *            tokenId:
+     *              type: number
+     *            metadata:
+     *              type: string
+     *            txHash:
+     *              type: string
+     *            assetType:
+     *              type: string
+     *            external_link:
+     *              type: string
+     *            description:
+     *              type: string
+     *            property:
+     *              type: string
+     *            mint_price:
+     *              type: number
+     *            price:
+     *              type: number
+     *            collectionId:
+     *              type: string
+     *            creator:
+     *              type: string
+     *            owner:
+     *              type: string
+     *       - name: id
+     *         description: id of item to be updated.
+     *         in: path
+     *         required: true
+     *         type: string
+     *     responses:
+     *       200:
+     *         description: Response contain success message.
+     *         schema:
+     *           type: object
+     *           properties:
+     *            message:
+     *              type: string
+     *       426:
+     *         description: "Indicate that version in header is not supported"
 
-   *       401:
-   *         description: "User don't have permissions to execute this API or token is not valid"
+     *       404:
+     *         description: "Indicate driver_id is not valid."
 
-   *       498:
-   *         description: "Indicate Auth token is invalid or Expired"
+     *       401:
+     *         description: "User don't have permissions to execute this API or token is not valid"
 
-   */
-  app.put("/api/v1/items/update/:id", controller.update);
+     *       498:
+     *         description: "Indicate Auth token is invalid or Expired"
 
-  app.get("/api/v1/items/:collectionId/:tokenId", controller.one);
+     */
+    app.put("/api/v1/items/update/:id", controller.update);
+
+    app.get("/api/v1/items/:collectionId/:tokenId", controller.one);
 
 };
